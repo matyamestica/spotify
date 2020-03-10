74e29277eb9c35097e8e486f1841e02a88a1b02d
maty <maty6289@live.cl>
2020-03-10 00:43:34 -0300

    actualizar artistas

--- controllers/artist.js
+++ controllers/artist.js
@@ -71,8 +71,26 @@ function saveArtist(req,res){
     })
 }
 
+function updateArtist(req,res) {
+    var artistId = req.params.id;
+    var update = req.body;
+
+    Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated) => {
+        if(err){
+            res.status(500).send({message:'Error al guardar el artista'});
+        }else{
+            if(!artistUpdated){
+                res.status(404).send({message:'El artista no ha sido actualizado'});
+            }else{
+                res.status(200).send({artist:artistUpdated});
+            }
+        }
+    })
+}
+
 module.exports = {
     getArtist,
     saveArtist,
-    getArtists
+    getArtists,
+    updateArtist
 };
