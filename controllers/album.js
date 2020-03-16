39be59724275e342e4444966f33cd91d43631b65
maty <maty6289@live.cl>
2020-03-16 19:47:35 -0300

    Actualizar album
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>

--- controllers/album.js
+++ controllers/album.js
@@ -32,7 +32,17 @@ function getAlbums(req,res){
     }else{
         var find = Album.find({artist: artisId}).sort('year');
     }
-
+    find.populate({path: 'artist'}).exec((err, albums) => {
+        if(err){
+            res.status(500).send({message:' Error en la peticion'});
+        }else{
+            if(!albums){
+                res.status(404).send({message:' No Hay albums'});
+            }else{
+                res.status(200).send({albums});
+            }
+        }
+    })
 }
 
 function saveAlbum(req,res){
@@ -59,9 +69,27 @@ function saveAlbum(req,res){
     });
 }
 
+function updateAlbum(req,res){
+    var albumId = req.params.id;
+    var update = req.body;
+
+    Album.findByIdAndUpdate(albumId,update,(err,albumUpdated) => {
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else{
+            if(!albumUpdated){
+                res.status(404).send({message:'No se ha actualizado el album'});
+            }else{
+                res.status(200).send({album: albumUpdated});
+            }
+        }
+    });
+}
 
 
 module.exports = {
     getAlbum,
-    saveAlbum
+    saveAlbum,
+    getAlbums,
+    updateAlbum
 }
