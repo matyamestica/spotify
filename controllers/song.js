d82921b5e80ae9db0b3b4e4de82da36cee6aac4b
Marco Castillo <marco.cgonzalez19@gmail.com>
2020-03-16 18:23:44 -0300

    Método para crear canciones en un album
    Se agregaron las funciones para ir guardando canciones en un determinado album
    
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>

--- controllers/song.js
+++ controllers/song.js
@@ -12,6 +12,30 @@ function getSong(req, res){
     res.status(200).send({message: 'Controlador cancion'});
 }
 
+function saveSong(req, res){
+    var song = new Song();
+
+    var params = req.body;
+    song.number = params.number;
+    song.name = params.name;
+    song.duration = params.duration;
+    song.file = null;
+    song.album = params.album;
+
+    song.save((err, songStored) => {
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else{
+            if(!songStored){
+                res.status(404).send({message: 'No se ha guardado la canción'});
+            }else{
+                res.status(200).send({song: songStored});
+            }
+        }
+    });
+}
+
 module.exports = {
-    getSong
+    getSong,
+    saveSong
 };
--- routes/song.js
+++ routes/song.js
@@ -12,7 +12,7 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/song',md_auth.ensureAuth, SongController.getSong);
-
+api.post('/song',md_auth.ensureAuth, SongController.saveSong);
 
 
 module.exports = api;
