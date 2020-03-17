df21d88a36407f5cf671f5d133090e8f06003f7a
Barbarasuasep <barbarasuarezsepulveda@gmail.com>
2020-03-17 16:00:32 -0300

    Servicio de Usuario

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,44 @@
 import { User } from './models/user';
-import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'MUSIFY';
   public user: User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(
+  	private _userService:UserService
+  ){
     this.user = new User('','','','','','ROLE_USER','','','');
+  }
+
+  ngOnInit(){
+
   }
 
   public onSubmit(){
   	console.log(this.user);
+
+  	this._userService.signup(this.user).subscribe(
+  		response => {
+  			console.log(response);
+  		},
+  		error => {
+  			var errorMessage = <any>error;
+
+  			if(errorMessage != null){
+  				console.log(error);
+  			}
+  		}
+
+  	)
   }
 
 }
