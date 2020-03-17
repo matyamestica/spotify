a0ca72dfde1c4bf56bf5534f12c81141e633206c
maty <maty6289@live.cl>
2020-03-17 11:44:17 -0300

    modelos creados en la vista
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>
    Co-Authored-By: matiasamesticaretamal <matiasamesticaretamal@users.noreply.github.com>
    Co-Authored-By: mcastillogonzalez <mcastillo19@users.noreply.github.com>

---
+++ client/src/app/models/song.ts
@@ -0,0 +1,9 @@
+export class Song{
+  constructor(
+    public number: string,
+    public name: string,
+    public duration: string,
+    public file: string,
+    public album: string
+  ){}
+  }
---
+++ client/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User{
+  constructor(
+    public _id: string,
+    public name: string,
+    public surname: string,
+    public email: string,
+    public password: string,
+    public role: string,
+    public image: string,
+    public song: string,
+    public segundo: string
+  ){}
+  }
