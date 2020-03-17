821cad547dc17d8892d884b30ce951c1c38db431
maty <maty6289@live.cl>
2020-03-17 19:23:18 -0300

    revirtiendo cambios

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { User } from './models/user';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'MUSIFY';
+  public title = 'MUSIFY';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+    this.user = new User('','','','','','ROLE_USER','','','');
+  }
+
 }
