d31cf16b4e238044191ff24435272f8f3b8d875d
maty <maty6289@live.cl>
2020-03-18 03:46:13 -0300

    login y logout funcionando
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>
    Co-Authored-By: mcastillo007 <mcastillo007@users.noreply.github.com>

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public errorMessage;
 
   constructor(
     private _userService: UserService
@@ -21,23 +22,79 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken;
 
+    console.log(this.identity);
+    console.log(this.token);
 
   }
 
   public onSubmit() {
     console.log(this.user);
+    //Conseguir datos del usuario identificado
     this._userService.signup(this.user).subscribe(
       response => {
-        console.log(response);
+        let identity = response.user;
+        this.identity = identity;
+
+        if(!this.identity._id){
+          alert("El usuario no esta correctamente identificado");
+        }else{
+          //Crear elemento en el local storage para tener al usuario en sesion
+
+          localStorage.setItem('identity',JSON.stringify(identity));
+
+          //conseguir el tokken para enviarselo a la peticion http
+
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+
+              if(this.token.length <= 0){
+                alert("El token no se ha generado");
+              }else{
+                //Crear elemento en el local storage para tener el token disponible
+                localStorage.setItem('token',token);
+
+                console.log(token);
+                console.log(identity);
+
+              }
+
+            },
+            error => {
+              var errorMessage = <any>error;
+
+              if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.message;
+
+                console.log(error);
+              }
+            }
+          );
+        }
 
       },
       error => {
         var errorMessage = <any>error;
+
         if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
+
           console.log(error);
         }
       }
     );
+  }
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
   }
 }
