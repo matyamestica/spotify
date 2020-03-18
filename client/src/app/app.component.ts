0e43aa4f1d94a9b452db40e816f1089928594131
Marco Castillo <marco.cgonzalez19@gmail.com>
2020-03-18 04:26:16 -0300

    Haciendo funcionar el login
    Se habilitan formularios de identificación y registro de usuarios, funcionando correctamente.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,34 +10,87 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   public title = 'MUSIFY';
   public user: User;
+  public user_register: User;
   public identity;
   public token;
+  public errorMessage;
 
   constructor(
     private _userService: UserService
 
   ) {
     this.user = new User('', '', '', '', '', 'ROLE_USER', '', '', '');
+    this.user_register = new User('', '', '', '', '', 'ROLE_USER', '', '', '');
   }
 
   ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
 
-
+    console.log(this.identity);
+    console.log(this.token);
   }
 
   public onSubmit() {
     console.log(this.user);
+    //Conseguir los datos del usuario identificado
     this._userService.signup(this.user).subscribe(
       response => {
-        console.log(response);
+        let identity = response.user;
+        this.identity = identity;
 
+        if(!this.identity._id){
+          alert("El usuario no está correctamente identificado");
+        }else{
+          //Crear elemento en el localstorage para tener al usuario en sesión
+            localStorage.setItem('identity', JSON.stringify(identity));
+          //Conseguir el token para enviarselo a cada petición http
+                    this._userService.signup(this.user, 'true').subscribe(
+                      response => {
+                        let token = response.token;
+                        this.token = token;
+                
+                        if(this.token.length <= 0){
+                          alert("El token no se ha generado correctamente");
+                        }else{
+                          //Crear elemento en el localstorage para tener token disponible
+                          localStorage.setItem('token', token);
+
+                            console.log(token);
+                            console.log(identity);
+                
+                        }
+                      },
+                      error => {
+                        var errorMessage = <any>error;
+                        if(errorMessage != null){
+                          var body = JSON.parse(error._body);
+                          this.errorMessage = body.message;
+                          console.log(error);
+                        }
+                      }
+                    );
+
+        }
       },
       error => {
         var errorMessage = <any>error;
         if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
           console.log(error);
         }
       }
     );
+  }
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
+  onSubmitRegister(){
+    console.log(this.user_register);
   }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -7,6 +7,8 @@ import { GLOBAL } from './global';
 @Injectable()
 export class UserService {
 
+  public identity;
+  public token;
   public url: string;
 
  constructor(private _http: Http) {
@@ -18,7 +20,6 @@ export class UserService {
       user_to_login.gethash = gethash;
     }
 
-
     let json = JSON.stringify(user_to_login);
     let params = json;
 
@@ -27,4 +28,29 @@ export class UserService {
     return this._http.post(this.url + 'login', params, {headers: headers})
                      .map(res => res.json());
   }
+
+  getIdentity(){
+    let identity = JSON.parse(localStorage.getItem('identity'));
+
+    if(identity != "undefined"){
+        this.identity = identity;
+    }else{
+      this.identity = null;
+
+    }
+    return this.identity;
+  }
+  getToken(){
+    let token = localStorage.getItem('token');
+
+    if(token != "undefined"){
+      this.token = token;
+    }else{
+      this.token = null;
+    }
+
+    return this.token;
+
+  }
+
 }
