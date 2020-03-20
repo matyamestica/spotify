28f5f6b04d76dfcdc598998223db9ebff53a12f3
maty <maty6289@live.cl>
2020-03-19 21:39:51 -0300

    actualizando la imagen del usuario
    Co-Authored-By: mcastillo007 <mcastillo007@users.noreply.github.com>
    Co-Authored-By: barbarasuasep <barbarasuasep@users.noreply.github.com>

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,6 +1,8 @@
+import { GLOBAL } from './../services/global';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../services/user.service';
 import { User } from './../models/user';
+
 
 
 
@@ -16,16 +18,18 @@ export class UserEditComponent implements OnInit{
   public identity;
   public token;
   public alertMessage;
+  public url: string;
 
   constructor(
     private _userService: UserService
   ){
 
     this.titulo = 'Actualizar mis datos';
-  
+
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.user =this.identity;
+    this.url = GLOBAL.url;
   }
   ngOnInit(){
     console.log('user-edit.component.ts cargado');
@@ -42,7 +46,22 @@ export class UserEditComponent implements OnInit{
           //this.user = response.user;
           localStorage.setItem('identity', JSON.stringify(this.user));
           document.getElementById("identity_name").innerHTML = this.user.name;
+
+
+          if(!this.filesToUpload){
+            //redireccion
+          }else{
+            this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload).then(
+              (result: any) => {
+                this.user.image = result.image;
+                localStorage.setItem('identity', JSON.stringify(this.user));
+
+                console.log(this.user);
+              }
+            );
+          }
           this.alertMessage = 'Datos actualizados correctamente';
+
         }
       },
       error => {
@@ -54,5 +73,40 @@ export class UserEditComponent implements OnInit{
         }
       }
     );
+  }
+
+  // tslint:disable-next-line:member-ordering
+  public filesToUpload: Array<File>;
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload);
+  }
+
+  makeFileRequest(url: string, params: Array <string>, files: Array<File>){
+    var token = this.token;
+
+    return new Promise(function(resolve, reject){
+      var formData:any = new FormData();
+       var xhr = new XMLHttpRequest();
+
+       for(var i = 0; i < files.length; i++){
+         formData.append('image',files[i], files[i].name);
+       }
+       xhr.onreadystatechange = function(){
+         if(xhr.readyState == 4){
+          if(xhr.status == 200){
+            resolve(JSON.parse(xhr.response));
+          }else{
+            reject(xhr.response);
+          }
+         }
+       }
+
+       xhr.open('POST', url, true);
+       xhr.setRequestHeader('Authorization', token);
+       xhr.send(formData);
+    });
+
   }
 }
